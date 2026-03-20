786f48f8432fdca8f200ebd45596e8223acaaadc
MikelSL9 <mikelreguers@gmail.com>
2026-03-20 21:23:34 +0100

    chore: initial project setup

---
+++ src/main/java/com/mikelsl9/supplierordermanager/SupplierOrderManagerApplication.java
@@ -0,0 +1,13 @@
+package com.mikelsl9.supplierordermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplierOrderManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupplierOrderManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mikelsl9/supplierordermanager/SupplierOrderManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikelsl9.supplierordermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplierOrderManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
